3fb79965cc7abb421f9d1c314e791dc10f813a2e
Elisaveta <elisaveta.s.hristova@gmail.com>
2018-12-13 19:03:37 +0200

    Buttons for delete are added. Todos have checkboxes.

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -15,12 +15,20 @@ const saveNotes = function(notes){
 
 //Generate the DOM structure for a note
 const generateNoteDOM = function(note) {
-    const noteEl = document.createElement("p");
+    const noteEl = document.createElement("div");
+    const textEl = document.createElement("span");
+    const button = document.createElement("button");
+    //Set delete note btn
+    button.textContent = "x";
+    noteEl.appendChild(button);
+
     if(note.title.length > 0) {
-        noteEl.textContent = note.title;
+        textEl.textContent = note.title;
     } else {
-        noteEl.textContent = "Unnamed note"
+        textEl.textContent = "Unnamed note"
     }
+    //Set note text
+    noteEl.appendChild(textEl);
     return noteEl;
 }
 
--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -35,8 +35,23 @@ function renderTodos(todos, filters) {
 
 //Get the DOM elements for an individual todo
 const generateTodoDOM = function(todo) {
-    const todoEl = document.createElement("p");
-    todoEl.textContent = todo.text;
+    const todoEl = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const todoText = document.createElement('span');
+    const removeButton = document.createElement('button');
+
+    // Setup todo checkbox
+    checkbox.setAttribute('type', 'checkbox');
+    todoEl.appendChild(checkbox);
+
+    // Setup the todo text
+    todoText.textContent = todo.text;
+    todoEl.appendChild(todoText);
+
+    // Setup the remove button
+    removeButton.textContent = 'x';
+    todoEl.appendChild(removeButton);
+
     return todoEl;
 }
 
